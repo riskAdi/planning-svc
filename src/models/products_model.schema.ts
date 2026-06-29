ff3b4a62f902ce22e1ad855532b351d16565c46e
amjad <76674070+muhammad-amjad-kap@users.noreply.github.com>
2026-06-29 16:20:52 +0500

    fixed multi relation issue

--- src/models/products_model.schema.ts
+++ src/models/products_model.schema.ts
@@ -2,14 +2,19 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument } from 'mongoose';
 import mongoose from 'mongoose';
 
+import { ColorsClass } from './colorsClass.schema';
 import { ProductModelSize } from './productModelSize.schema';
 
 export type ProductsModelDocument = HydratedDocument<ProductsModel>;
 
 @Schema({ timestamps: true })
 export class ProductsModel {
-  @Prop({ required: false })
-  colors: string;
+  @Prop({
+    required: false,
+    type: mongoose.Schema.Types.ObjectId,
+    ref: ColorsClass.name,
+  })
+  colors: mongoose.Types.ObjectId | ColorsClass;
 
   @Prop({ required: false, type: mongoose.Schema.Types.Mixed })
   images: any;
